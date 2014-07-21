bf12960811f3aad5ec5e92c93500e57f1a93ed80
Nattaphat K.@home <nattaphat@gmail.com>
2014-07-21 22:23:24 +0700

    Dev

--- ex_html.py
+++ ex_html.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import os
 import pycurl
 
 def getDefp():
-	url = '/data/html/www/haii/gprocument/dept.txt'
-	tmp = '/data/html/www/haii/gprocument/tpm_dep.txt'
+	url = '/tmp/dept.txt'
+	tmp = '/tmp/tpm_dep.txt'
 	command ="awk -v lines=1 '/clickDept/ {for(i=lines;i;--i)getline; print $0 }' "+url+" > "+tmp;
 
 	os.system(command)
@@ -21,23 +21,26 @@ def getDefp():
 			print org_name
 
 def curlText():
+	url = "https://process.gprocurement.go.th/EGPWeb/jsp/control.egp"
 	c = pycurl.Curl()
-	c.setopt(c.URL, 'http://myappserver.com/ses1')
+	c.setopt(c.URL, url)
 	c.setopt(c.CONNECTTIMEOUT, 5)
 	c.setopt(c.TIMEOUT, 8)
-	c.setopt(c.POSTFIELDS, 'pizza=Quattro+Stagioni&extra=cheese')
+	c.setopt(c.USERAGENT, '')#'Foo\xa9'.encode('iso-8859-1'))
+	c.setopt(c.POSTFIELDS, 'ORG_ID=1&ORG_NAME=1&ORG_UNIT_ID=2&ORG_UNIT_NAME=&QUERY=&PROV_ID=&AMP_ID=&MOI_ID=&MINISTRY_CODE=&flag=SEARCH&servlet=gojsp&proc_id=FAGN0103H&proc_name=Agency&url=agc%2F&hidPROV_ID=&hidAMP_ID=&hidMOI_ID=&hidMINIS_ID=')
 	c.setopt(c.COOKIEFILE, '')
 	c.setopt(c.FAILONERROR, True)
-	c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
+	c.setopt(c.HTTPHEADER, ['Content-type: application/x-www-form-urlencoded;charset=tis-620'])
+
 	try:
-	    c.perform()
+	    # c.perform()
 	 
-	    c.setopt(c.URL, 'http://myappserver.com/ses2')
-	    c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
+	    # c.setopt(c.URL, 'http://myappserver.com/ses2')
+	    # c.setopt(c.POSTFIELDS, 'foo=bar&bar=foo')
 	    c.perform()
 	except pycurl.error, error:
 	    errno, errstr = error
 	    print 'An error occurred: ', errstr
-getDefp()
 
-
+# getDefp()
+curlText()
